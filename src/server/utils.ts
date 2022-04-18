4479474126e679584f59fa639e980c1eac6afa05
Okan YILDIRIM <63511519+ragokan@users.noreply.github.com>
2022-04-18 14:50:05 +0300

    do smt

--- src/server/utils.ts
+++ src/server/utils.ts
@@ -2,20 +2,22 @@ import type { HTTPMethods } from "fastify";
 import type { IMeta, ITarget } from "./types";
 
 export const getMeta = (target: any): IMeta => {
-  if (!target.__decorator_meta__) {
-    target.__decorator_meta__ = {
-      url: "",
-      routes: {},
-    } as IMeta;
-  }
+  target.__decorator_meta__ ??= {
+    url: "",
+    routes: {},
+  } as IMeta;
   return target.__decorator_meta__;
 };
 
 export const createRouteDecorator =
-  <T>(method: HTTPMethods, path: string, ...propertyDecorators: PropertyDecorator[]): PropertyDecorator =>
+  <T>(
+    method: HTTPMethods,
+    path: string,
+    ...propertyDecorators: PropertyDecorator[]
+  ): PropertyDecorator =>
   (target, functionKey) => {
     const meta = getMeta(target as ITarget<T>);
-    meta.routes[functionKey] = {
+    meta.routes[functionKey] ??= {
       method: method as any,
       url: path,
       functionKey,
